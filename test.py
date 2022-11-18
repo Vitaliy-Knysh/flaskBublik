89e293989a7f739942023b484c6cd16adbb4e5d4
Vitaliy <mr.knyshjr2@gmail.com>
2022-11-18 20:15:43 +0300

    2 одинаковых бублика, добавил таблицу сверху

--- test.py
+++ test.py
@@ -4,15 +4,29 @@ app = Flask(__name__)
 from flask import jsonify, request, render_template
 import time
 from threading import Thread
+from elasticsearch import Elasticsearch
 
+'''@app.route('/')
+def hello():
+    return render_template('sunburst-chart.html', numbers=[88, 84, 77, 66, 55, 50, 34],
+                           names=['1', '2', '3', '4', '5', '6', '7'])'''
+
+val = [10, 14, 4, 6, 10, 4]
 
 @app.route('/')
 def hello():
-    return render_template('pie-chart.html', numbers=[88, 84, 77, 66, 55, 50, 34],
-                           names=['Укропу', 'Кошачью жопу', 'Картошек', 'Мандавошек', 'Воды', 'Хуёв туды', 'Дров'])
+    return render_template('sunburst-chart.html',
+                           ids=["Шуя", "Тверь", "Шуя - PASS", "Шуя - FAIL", "Тверь - PASS", "Тверь - FAIL"],
+                           labels=["Шуя", "Тверь", "PASS " + '<br>' + str(round(100 * (val[2] / val[0]))) + '%',
+                                   "FAIL" + '<br>' + (str(round(100 * val[3] / val[0]))) + '%',
+                                   "PASS" + '<br>' + (str(round(100 * val[4] / val[1]))) + '%',
+                                   "FAIL" + '<br>' + (str(round(100 * val[5] / val[1]))) + '%'],
+                           parents=["", "", "Шуя", "Шуя", "Тверь", "Тверь"],
+                           values=val)
 
 
 if __name__ == '__main__':
+    # es = Elasticsearch(address="172.26.24.198", port="9200", login="elastic", password="1qazXSW@")
+    # print(es.ping())
     # new_thread.start()
-    app.debug = True
-    app.run()
+    app.run(debug=True)
